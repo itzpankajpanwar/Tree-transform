f4dbfc4d802568f48b60b7228cfcded62bd06344
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-04-22 01:07:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def parse():
     parser.add_argument('-train', action='store_true',help='whether train the model')
     parser.add_argument('-test', action='store_true',help='whether test')
     parser.add_argument('-valid_path',default='data/valid.txt',help='validation data path')
-    parser.add_argument('-train_path',default='data/train.txt',help='training data path')
-    parser.add_argument('-test_path',default='data/test.txt',help='testing data path')
+    parser.add_argument('-train_path',default='/content/Tree-transform/data/train.txt',help='training data path')
+    parser.add_argument('-test_path',default='/content/Tree-transform/data/test.txt',help='testing data path')
     args = parser.parse_args()
     
     return args
